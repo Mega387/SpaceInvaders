ae48bec13327bdc261902c033a0790a78a628459
ITHUB\Student <qew>
2024-10-15 19:47:09 +0300

    space
    updete
    
    fix bagov

--- SpaceInvaders/Assets/_Source/Bullet.cs
+++ SpaceInvaders/Assets/_Source/Bullet.cs
@@ -3,15 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float speedBullet = 1f;
+
+
     void Start()
     {
-        
+        if (gameObject.CompareTag("Bullet"))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speedBullet);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
         
